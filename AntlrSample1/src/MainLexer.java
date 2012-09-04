fcaa619e0fe26aa8f2c45239b9f77dcfc010a947
Masahiro Tateno <tateno.masahiro@dena.jp>
2012-09-04 00:06:55 -0700

    replace space to tab

--- AntlrSample1/src/MainLexer.java
+++ AntlrSample1/src/MainLexer.java
@@ -5,63 +5,63 @@ import java.util.ArrayList;
 import org.antlr.runtime.*;
  
 public class MainLexer {
-    public static void main(String[] args) {
-        String[] tests = {
-            	"hoge.fuga",
-            	"/hoge/.func('hoge hoge')/2",
-            	"3/2",
-            	"hoge/2",
-            	"3//hoge/",
-            	"/* hoge */",
-            	"3/ /hoge/",
-            	"log hoge;",
-            	"console.log('hoge');",
-            	"log hoge;console.log('hoge');",
-            	"{ log hoge; console.log('hoge');/hoge/g;3 / 2;}"
-        };
-    	ArrayList<String> list = getTokenList();
-        for( int i = 0; i < tests.length; ++i ) {
-        	String test = tests[i];
-        	System.out.println("TEST: "+test);
-            try {
-               CharStream input = new ANTLRStringStream(test);
-               L lexer = new L();
-               lexer.setCharStream(input);
-               Token token;
-               while ( true ) {
-            	   token = lexer.nextToken();
-            	   if( token.getType() == Token.EOF ) break;
-                   System.out.println("Token: "+token.getText() + "("+list.get(token.getType())+")");
-               }
-            } catch(Throwable t) {
-               System.out.println("Exception: "+t);
-               t.printStackTrace();
-            }
-            System.out.println();
-        }
-    }
+	public static void main(String[] args) {
+		String[] tests = {
+			"hoge.fuga",
+			"/hoge/.func('hoge hoge')/2",
+			"3/2",
+			"hoge/2",
+			"3//hoge/",
+			"/* hoge */",
+			"3/ /hoge/",
+			"log hoge;",
+			"console.log('hoge');",
+			"log hoge;console.log('hoge');",
+			"{ log hoge; console.log('hoge');/hoge/g;3 / 2;}"
+		};
+		ArrayList<String> list = getTokenList();
+		for( int i = 0; i < tests.length; ++i ) {
+			String test = tests[i];
+			System.out.println("TEST: "+test);
+			try {
+				CharStream input = new ANTLRStringStream(test);
+				L lexer = new L();
+				lexer.setCharStream(input);
+				Token token;
+				while ( true ) {
+					token = lexer.nextToken();
+					if( token.getType() == Token.EOF ) break;
+					System.out.println("Token: "+token.getText() + "("+list.get(token.getType())+")");
+				}
+			} catch(Throwable t) {
+				System.out.println("Exception: "+t);
+				t.printStackTrace();
+			}
+			System.out.println();
+		}
+	}
 
 	private static ArrayList<String> getTokenList() {
 		ArrayList<String> list = new ArrayList<String>();
-        try {
-            FileReader f = new FileReader("src/L.tokens");
-            BufferedReader b = new BufferedReader(f);
-            String s;
-            while((s = b.readLine())!=null){
-                String[] ary = s.split("=");
-                int idx = Integer.parseInt(ary[1]);
-                String val = ary[0];
-                if( list.size() <= idx) {
-             	   while ( list.size() <= idx) {
-             		   list.add("");
-             	   }
-                }
-                list.set( idx, val );
-            }
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
-        return list;
+		try {
+			FileReader f = new FileReader("src/L.tokens");
+			BufferedReader b = new BufferedReader(f);
+			String s;
+			while((s = b.readLine())!=null){
+				String[] ary = s.split("=");
+				int idx = Integer.parseInt(ary[1]);
+				String val = ary[0];
+				if( list.size() <= idx) {
+			 		while ( list.size() <= idx) {
+			 			list.add("");
+			 		}
+				}
+				list.set( idx, val );
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return list;
 	}
-    
+	
 }
